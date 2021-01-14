6a833e6c8559843d0cd61752dae08de5718af6b6
Ben Awad <benawad97@gmail.com>
2021-01-13 19:49:59 -0600

    styling

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,13 +7,14 @@ type Props = {
 };
 
 const Layout = ({ children, title = "This is the default title" }: Props) => (
-  <div>
+  <div className="flex">
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link rel="shortcut icon" href="/favicon.ico" />
     </Head>
-    {children}
+    <div className="mx-auto w-full max-w-screen-lg">{children}</div>
     {/* <footer>
       <hr />
       <span>I'm here to stay (Footer)</span>
--- pages/hex.tsx
+++ pages/hex.tsx
@@ -1,4 +1,5 @@
 import Layout from "../components/Layout";
+import "tailwindcss/tailwind.css";
 import { useDropzone } from "react-dropzone";
 import { useCallback, useState } from "react";
 import { saveAs } from "file-saver";
@@ -64,37 +65,48 @@ const IndexPage = () => {
   });
   return (
     <Layout title="TikTok Long Video Converter">
-      <div style={{ display: "flex" }}>
-        <div style={{ margin: "auto" }}>
-          <div style={{ marginTop: 20 }}>
-            This only supports webm videos currently, if your video is in
-            another format,{" "}
-            <a href="http://letmegooglethat.com/?q=convert+to+webm">
-              convert it first
-            </a>
-            .
-          </div>
+      <h1 className="text-4xl mt-8 mb-6">LongTok</h1>
+      <ul>
+        <li className="mb-4">
+          There is a way to upload videos longer than 1 minute to TikTok: &nbsp;
+          <a
+            className="text-blue-800"
+            href="https://www.tiktok.com/@nathan_ham/video/6917287908621749509?lang=en"
+          >
+            proof
+          </a>
+        </li>
+        <li>
+          1. &nbsp;
+          <a
+            className="text-blue-800"
+            href="http://letmegooglethat.com/?q=convert+to+webm"
+          >
+            Convert your video to webm format
+          </a>{" "}
+          (if you know how to use ffmpeg, you can do:{" "}
+          <code className="bg-gray-200 p-1">
+            ffmpeg -i myvideo.mov output.webm
+          </code>
+          )
+        </li>
+        <li>
+          2.
           {!loading ? (
-            <div {...getRootProps({ style: { padding: 40 } })}>
+            <div {...getRootProps()}>
               <input {...getInputProps()} />
-              {isDragActive ? (
-                <p>Drop the files here ...</p>
-              ) : (
-                <p>Drag 'n' drop some files here, or click to select files</p>
-              )}
+              <button className="mb-4 mr-2 py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none">
+                upload webm video
+              </button>
             </div>
           ) : (
             <div>loading... (this will take a little bit)</div>
           )}
-          {downloaded && (
-            <div>
-              conversion and download complete (make sure to upload the video
-              using TikTok's website NOT the app)
-            </div>
-          )}
+          {downloaded && <div>conversion and download complete</div>}
           {err && <div>Error: {err}</div>}
-        </div>
-      </div>
+        </li>
+        <li>3. Upload the new video to TikTok's website (NOT the app)</li>
+      </ul>
     </Layout>
   );
 };
